dbeb9cb05f0ffda20db16d9647b9a9fb166b970a
BobJohn69 <minhtuan030304@gmail.com>
2025-02-09 15:20:05 +0700

    Update workflows and add allowed host

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5172
+    port: 5172,
+    allowedHosts: ['hub.threemusketeer.click']
   }
 })
